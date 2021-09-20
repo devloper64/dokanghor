dc1407e007caee8841460c8dbba1de8c1f2aac67
coder <mdmahfuj010@gmail.com>
2021-09-20 22:21:43 +0600

    up to date

--- src/main/java/com/coder/ecommerce/service/customReponse/CustomLoginResponse.java
+++ src/main/java/com/coder/ecommerce/service/customReponse/CustomLoginResponse.java
@@ -1,9 +1,13 @@
 package com.coder.ecommerce.service.customReponse;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class CustomLoginResponse {
+
     private String idToken;
 
+    @JsonProperty("id_token")
     public String getIdToken() {
         return idToken;
     }
